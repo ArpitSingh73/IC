66ac42bbc8724d1154791df041eb7b29c389a373
ArpitSingh73 <arpitsingh73073@gmail.com>
2024-03-19 22:28:01 +0530

    addition for deployment .

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// // --------------------------deployment------------------------------
+
+
+
+
+
 //
 //
 app.post("/signup", async (req, res) => {
